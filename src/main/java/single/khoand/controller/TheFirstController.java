23b3f7b34a6db168b6051a6b9204a37e37f31997
khoand10@gmail.com <khoand10@gmail.com>
2018-04-19 12:47:10 +0700

    push hello world

--- src/main/java/single/khoand/controller/TheFirstController.java
+++ src/main/java/single/khoand/controller/TheFirstController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TheFirstController {
 	@RequestMapping("/")
-	@ResponseBody
+	/*@ResponseBody*/
 	public String loadIndex() {
-		return "pages/index";
+		return "index.jsp";
 	}
 }
